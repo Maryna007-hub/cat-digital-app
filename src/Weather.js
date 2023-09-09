1f9d0a6b2d5a63de62bda7f0088806aeadbd55bc
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-09 16:40:57 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -11,7 +11,7 @@ import {
           <form>
         <div className="row">
           <div className="col-9">
-            <input type="search" placeholder="Search a city" className="form-control" autocomplete="off"/>
+            <input type="search" placeholder="Search a city" className="form-control" autoComplete="off"/>
           </div>
           <div className="col-3">
            <input type="submit" value="Search" className="btn btn-primary" />
@@ -37,6 +37,12 @@ import {
         <UilWind size={19} className='wind'/>  Wind: 12 km/h
         </li>
     </ul>
+    <div className='row'>
+        <div className='col'>
+           <h2>Daily Temperature Evaluation</h2>
+            <hr/>
+        </div>
+    </div>
     </div>
   </div>
   </div>
